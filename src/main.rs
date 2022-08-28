1c4e0bf9e8072ced52c869954a56fb14f8a652e8
Bilal Abu Saad <bilalabusaad@gmail.com>
2022-08-28 19:50:33 -0400

    Simple binary that scrapes sthe brooklynmuseum careers page and prints out elemenets under '.job-postings li' that have Curator|curator in their text element Note that I'm not sure if this is the best way, since it's tailored for the brooklyn museum page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use scraper::{Html, Selector};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("https://www.brooklynmuseum.org/about/careers")?;
+    let document = Html::parse_document(&resp.text().unwrap());
+    let selector = Selector::parse(".job-postings li").unwrap();
+    let sections: Vec<_> = document.select(&selector).collect();
+    for sec in sections {
+        for t in sec.text().filter(|x| x.contains("curator") || x.contains("Curator")) {
+            println!("I see text: {:?}", t)
+        }
+    }
+    Ok(())
 }
