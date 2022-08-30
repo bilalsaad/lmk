aafb38550e680878bbdbcb16e52dbee051941aa3
Bilal Abu Saad <bilalabusaad@gmail.com>
2022-08-29 20:57:58 -0400

    mend

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use crate::myscraper::{Matcher, Target};
 mod myscraper;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let s = myscraper::Scraper::new(vec![Target {
-        uri: "https://www.brooklynmuseum.org/about/careers".to_string(),
-        matcher: Matcher::TextMatch("urator".to_string()),
-    },
-    Target {
-        uri: "poop".to_string(),
-        matcher: Matcher::AnyChange
-    }
+    let s = myscraper::Scraper::new(vec![
+        Target {
+            uri: "https://www.brooklynmuseum.org/about/careers".to_string(),
+            matcher: Matcher::TextMatch("urator".to_string()),
+        },
+        Target {
+            uri: "poop".to_string(),
+            matcher: Matcher::AnyChange,
+        },
     ]);
     s.start()
 }
