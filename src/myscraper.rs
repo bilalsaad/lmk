eedf980e503927d1d626a0a33b3d89f56fc3585e
Bilal Abu Saad <bilalabusaad@gmail.com>
2022-08-30 22:42:49 -0400

    REfactored html page handling a bit - want to make it more testable, but I don't think I succeeded.

--- src/myscraper.rs
+++ src/myscraper.rs
@@ -27,6 +27,7 @@ impl Scraper {
 
     pub fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
         // make async
+        let selector = Selector::parse("*").unwrap();
         for t in &self.targets {
             let resp = match reqwest::blocking::get(&t.uri).map(|x| x.text()) {
                 Ok(Ok(x)) => Html::parse_document(&x),
@@ -35,7 +36,7 @@ impl Scraper {
                     continue;
                 }
             };
-            handle_page_content2(resp, &t.matcher);
+            handle_page_content(resp.select(&selector).flat_map(|x| x.text()), &t.matcher)?;
         }
         Ok(())
     }
@@ -43,15 +44,15 @@ impl Scraper {
 
 // Checks content for any matches. For each encountered match a notification event is generated.
 // Note that if content has not changed since last handling, no notifcations are generated.
-fn handle_page_content2(
-    content: Html,
+fn handle_page_content<'a, I>(
+    content: I,
     matcher: &Matcher,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    I: Iterator<Item = &'a str>,
+{
     // Look up old content and compare
-    let selector = Selector::parse("*").unwrap();
     content
-        .select(&selector)
-        .flat_map(|x| x.text())
         .filter_map(|x| {
             // custom matcher(s) for document id
             match &matcher {
